8452cbcb30acf3fd8529dd594544c0f7ecde87cf
Justin Makaila <justinmakaila@gmail.com>
2018-07-13 11:35:23 -0400

    Removed lodash dependency

--- src/getSymlinkedDependencies.js
+++ src/getSymlinkedDependencies.js
@@ -1,8 +1,9 @@
 const fs = require('fs')
-const _ = require('lodash')
 
 const isSymlink = dependency =>
     fs.lstatSync(`node_modules/${dependency}`).isSymbolicLink()
 
 module.exports = directory =>
-    _.keys(require(`${directory}/package.json`).dependencies).filter(isSymlink)
+    Object.keys(require(`${directory}/package.json`).dependencies || {}).filter(
+        isSymlink,
+    )
